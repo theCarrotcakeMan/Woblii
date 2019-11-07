2bf727cb0cc7def9aed16f02513ab579756aaa1f
John Falcon <falcon@walktopus.com>
2019-11-07 12:42:43 -0600

    I just ran out of the room --prog

--- themes/woblii-2019/page-login.php
+++ themes/woblii-2019/page-login.php
@@ -33,7 +33,7 @@
     <div id="loginWoblii" style="height: 100vh" class="flex justify-around flex-wrap items-center content-around py-48">
         <figure class="flex-1 w-full md:w-auto text-right">
             <img class="inline-block object-none object-contain object-right w-1/2 md:w-2/5 max-w-xs mr-10" src="<?php echo THEMEPATH."assets/images/woblii-logo.svg"; ?>" alt="Woblii">
-            <h2 class="flex-row block font-body text-purple-dark text-3xl font-semibold mr-10">Let's do this together</h2>
+            <h2 class="flex-row block font-body text-right text-purple-dark text-3xl font-semibold mr-10">Let's do this together</h2>
         </figure>
         <article class="flex-1 w-full md:w-auto">
             
