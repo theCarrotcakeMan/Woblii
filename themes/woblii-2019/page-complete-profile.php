05defd7fcc9d5a4153600c09e1193b05680e1907
John Falcon <john@pixel-muffin.com>
2020-03-11 11:19:30 -0600

    little comment

--- themes/woblii-2019/page-complete-profile.php
+++ themes/woblii-2019/page-complete-profile.php
@@ -1,14 +1,15 @@
 <?php
-    
+
     // Redirect user if already logged in
     if(!is_user_logged_in()){
         wp_redirect(site_url('/'));
     }
     $currentUser = wp_get_current_user();
-    
+
     if(!$currentUser)
         wp_redirect(site_url('signup'));
-    
+
+	// If user hasn't finished setting up their profile, redirect to complete profile page
     $userObject = fetchUserObject($currentUser);
     if(!empty($currentUser) && !empty($userObject->bio))
         wp_redirect(site_url('my-feed'));
@@ -19,7 +20,7 @@
         'loader' => new Mustache_Loader_FilesystemLoader(THEMEPATH.'/views'),
         'partials_loader' => new Mustache_Loader_FilesystemLoader(THEMEPATH.'/views/partials')
     ]);
-    
+
     $lines_terms = get_terms( [
         'taxonomy'      => 'line-of-business',
         'hide_empty'    => false,
@@ -30,6 +31,5 @@
 
     $tpl = $mustache->loadTemplate('full-profile');
     echo $tpl->render(['blueprint' => jsonToProp($userObject),'site_url' => site_url('/'),'lines' => jsonToProp($lines_terms),'user_role' => $userObject->role]);
-    
+
     get_footer();
-    
