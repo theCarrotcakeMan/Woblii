04e40efe051e5c7dd9e9c12513cfd5d313dc5cec
John Falcon <john@pixel-muffin.com>
2020-03-16 23:29:57 -0600

    Some spaces

--- themes/woblii-2019/page-login.php
+++ themes/woblii-2019/page-login.php
@@ -20,10 +20,10 @@
     // If request has error message, display as alert
      if($errorMessage): ?>
 
-    <div class="mx-auto w-4/5 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" style="top: 6rem;" role="alert">
+    <div class="mx-auto w-4/5 bg-green-100 border border-green-500 text-green-500 px-4 py-3 rounded relative" style="top: 8rem;" role="alert">
         <span class="block sm:inline"><?php echo $errorMessage; ?></span>
         <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-        <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Cerrar</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+        <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Cerrar</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
       </span>
     </div>
 
